21eeeb2d481c46c782babbb452ca5fd368f467a1
raulb98 <bucurraul123@yahoo.com>
2024-01-28 14:33:56 +0200

    Commit 6

--- src/Components/HomePage/home.js
+++ src/Components/HomePage/home.js
@@ -1,28 +1,33 @@
-import React, { useState } from 'react';
+import React, { useRef } from 'react';
 import Tab from 'react-bootstrap/Tab';
 import Tabs from 'react-bootstrap/Tabs';
 import Button from 'react-bootstrap/Button';
 import Form from 'react-bootstrap/Form';
 
 function HomePage() {
+    const appName = useRef(undefined);
+    const description = useRef(undefined);
 
     async function handleClick() {
         const tokenString = sessionStorage.getItem('token');
         const userToken = JSON.parse(tokenString);
         const token = userToken?.jwt;
         const username = userToken?.name;
-        console.log(username, token);
+        console.log(appName.current.value, description.current.value);
+
         return fetch('https://arbufe49zb.execute-api.eu-north-1.amazonaws.com/V1/create', {
             method: 'POST',
             headers: {
               'Content-Type': 'application/json'
             },
             body: JSON.stringify({
-              name: username,
+              name : username,
+              app_name: appName.current.value,
               token: token,
             })
           }).then(data => data.json())
     };
+
 
     const Display_Create_Tabs=()=> {
         const tokenString = sessionStorage.getItem('token');
@@ -36,16 +41,16 @@ function HomePage() {
                     className="mb-3"
                 >
                     <Tab eventKey="AppCreator" title="AppCreatorButton">
-                        <Form>
+                        <Form onSubmit={handleClick}>
                             <Form.Group className="mb-3" controlId="AppName">
                                 <Form.Label>Application Name</Form.Label>
-                                <Form.Control type="email" placeholder="Enter Application Name" />
+                                <Form.Control type="text" placeholder="Enter Application Name" ref={appName} />
                             </Form.Group>
                             <Form.Group className="mb-3" controlId="AppDescription">
                                 <Form.Label>Description</Form.Label>
-                                <Form.Control as="textarea" rows={3} />
+                                <Form.Control as="textarea" rows={3} ref={description} />
                             </Form.Group>
-                            <Button variant="primary" type="submit" onClick={handleClick}>
+                            <Button variant="primary" type="submit">
                                     Create
                             </Button>
                         </Form>
